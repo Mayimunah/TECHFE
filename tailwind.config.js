b00bc0652625d7084d1a66006c944ff16df42c0e
Mayimunah <4163113@myuwc.ac.za>
2024-02-02 20:24:38 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Poppins: "Poppins"
+    },
+    extend: {
+      colors: {
+        Teal: "#2F6C6D",
+        HummingBird: "#d1f1ee",
+        yellow: "#e4d63b",
+        Solitude: "#9E9EEB",
+        gray: "#000008"
+      },
+      animation: {
+        slide: "slide 25s linear infinite"
+      },
+      keyframes: {
+        slide: {
+          "0%,100%" : {transform: "translateX(5%)"},
+          "50%": {transform: "translateX(-120%)"}
+        }
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+}
